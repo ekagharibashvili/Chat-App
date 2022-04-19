5d652220eb8b06f59b5ff0efcf9ab80a2b4a8547
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2022-04-19 15:23:21 +0400

    create get message service

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname))
+
+const messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'}
+]
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
 const server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 });
